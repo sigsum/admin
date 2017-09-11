78202260f159d732535fe935868e880300f4f886
Linus Nordberg <linus@nordberg.se>
2017-09-11 15:07:26 +0200

    a script

--- bin/ietf-sync.sh
+++ bin/ietf-sync.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
-rsync -az $1 server.nordu.net:/a/ftp/in-notes ~/usr/share/ietf/
-rsync -az $1 server.nordu.net:/a/ftp/internet-drafts ~/usr/share/ietf/
-
+DELETE="--delete-excluded --delete"
+EXCLUDE="--exclude '*.p7s' --exclude '*.xml' --exclude '*.pdf'"
+rsync -az $EXCLUDE $DELETE $1 server.nordu.net:/a/ftp/in-notes ~/usr/share/ietf/
+rsync -az $EXCLUDE $DELETE $1 server.nordu.net:/a/ftp/internet-drafts ~/usr/share/ietf/
