514c9f829a111b414f2a9caefcfc2be75d9df013
Linus Nordberg <linus@nordberg.se>
2018-01-15 22:45:09 +0100

    ietf-sync

--- bin/ietf-sync.sh
+++ bin/ietf-sync.sh
@@ -1,5 +1,1 @@
-#! /bin/sh
-DELETE="--delete-excluded --delete"
-EXCLUDE="--exclude '*.p7s' --exclude '*.xml' --exclude '*.pdf'"
-rsync -az $EXCLUDE $DELETE $1 server.nordu.net:/a/ftp/in-notes ~/usr/share/ietf/
-rsync -az $EXCLUDE $DELETE $1 server.nordu.net:/a/ftp/internet-drafts ~/usr/share/ietf/
+/home/linus/src/ietf-sync.sh
