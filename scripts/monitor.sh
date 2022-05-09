7d62db016d6c4fc62263ddff840f2e69f7b23e1c
anweshadas <anwesha@das.community>
2022-05-09 16:12:58 +0200

    Adds the initial email code on error

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -7,15 +7,10 @@ do
 echo $url #this is for testing purpose. To be removed in thhe final code.
   response=$(curl -s -w "%{http_code}" $url)
   http_code=$(tail -n1 <<< "$response")
+  domain_name=$(echo $url | awk -F[/:] '{print $4}')
   if [[ "$http_code" != 200 ]]; then
-    echo Fatal. $url is down. status_code $http_code
-    domain_name=$(echo $url | awk -F[/:] '{print $4}')
-    echo $domain_name
-    # 1. Find IP address via dig
-    # 2. Check if we can ping the ip address
-    ip_address=$(dig +short $domain_name)
-    echo $dig_response #this is for testing purpose. To be removed in thhe final code.
-    #ping_response=$(ping $ip_address)
+    msg="Warning: $url is down. status_code $http_code"
+    echo $msg | mail -s "Warning: $domain_name is down" anwesha@verkligendata.se
   else
     echo $url is working.  status_code $http_code
    fi
