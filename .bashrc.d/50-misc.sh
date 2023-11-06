b3da47362512dbdbf04e60cc37135a3cf8e82523
Linus Nordberg <--show-origin>
2023-11-06 08:21:26 +0100

    add config file for ripgrep (rg)

--- .bashrc.d/50-misc.sh
+++ .bashrc.d/50-misc.sh
@@ -2,3 +2,4 @@ export GOPATH="$HOME/usr/go"
 export WORKON_HOME="$HOME/.virtualenvs"
 export GNUPGHOME="$HOME/.gnupg"
 export EDITOR=/usr/bin/emacs
+export RIPGREP_CONFIG_PATH=$HOME/.config/ripgreprc
